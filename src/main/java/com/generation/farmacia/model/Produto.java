11ecfc42846d4190df42e22b05b41f380e26a57a
Fabio Martins <generation@mail.com>
2023-03-20 21:58:53 -0300

    Atividade Farmacia Finalizada

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,78 @@
+package com.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 5, max = 50, message = "O atributo nome é obrigatorio")
+	private String nome;
+	
+	@NotBlank
+	@Size(min = 5, max = 50, message = "O atributo marca é obrigatorio")
+	private String marca;
+	
+	@NotNull(message = "O atributo quantidade é obrigatorio")
+	private Integer quantidade;
+	
+	@NotNull(message = "O atributo preço é obrigatorio")
+	private Double preco;
+	
+	@ManyToOne
+    @JsonIgnoreProperties("produto")
+    private Categoria categoria;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+}
